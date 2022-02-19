c150ac6af9afadc9308cbbea50f9f07b92df8bdf
Epeat <44922559+epeat96@users.noreply.github.com>
2022-02-19 07:18:35 -0300

    feat: add adding todos functionality and upload assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+
+  todoList: Todo[] = [];
+
+  setCheck(event: any) {
+    this.todoList[event.source.id].checked = event.checked;
+  }
+
+  countNotChecked() {
+    return this.todoList.filter(todo => !todo.checked).length;
+  }
+
+  keyFunc(event: any) {
+    if (event.key === "Enter") {
+      let text = event.target.value
+      if (text.length > 0) {
+        this.todoList.push(this.newTodo(this.todoList.length, event.target.value, false));
+      }
+    }
+  }
+
+  newTodo(id: number, todo: string, checked: boolean) {
+    return { id: id, todo: todo, checked: checked }
+  }
+
+  isTodoListNotEmpty() {
+    return this.todoList.length !== 0;
+  }
+
 }
